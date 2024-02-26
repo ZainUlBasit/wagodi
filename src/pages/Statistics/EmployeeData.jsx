3288bd8048fb1cf285b8cca03a927935603d703b
Zain Ul Basit <zainulbasit486@gmail.com>
2024-02-26 20:15:31 +0500

    employe station drop down added

--- src/pages/Statistics/EmployeeData.jsx
+++ src/pages/Statistics/EmployeeData.jsx
@@ -1,18 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DateInput from "../../components/Input/DateInput";
 import { BsSearch } from "react-icons/bs";
 import { FaArrowLeft } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 import EmployeeTable from "../../components/Tables/EmployeeTable";
+import AuthInputPopOver from "../../components/Input/AuthInputPopOver";
+import { Popover, Typography } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchStations } from "../../store/Slices/StationSlice";
 
 const EmployeeData = () => {
   const [SearchText, setSearchText] = useState("");
+  const [StationId, setStationId] = useState("");
+  const [StationName, setStationName] = useState("");
   const [CurDate, setCurDate] = useState("");
+
+  const dispatch = useDispatch();
+  const StationsData = useSelector((state) => state.StationReducer);
+  const Auth = useSelector((state) => state.auth);
+  useEffect(() => {
+    dispatch(fetchStations(Auth.data.companyId));
+  }, []);
+
   const navigate = useNavigate();
   const handleArrowLeftClick = () => {
     // Navigate to the last visited route
     navigate(-1);
   };
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+  const id = open ? "simple-popover" : undefined;
   return (
     <>
       <div className="flex flex-col justify-center items-center">
@@ -34,6 +57,71 @@ const EmployeeData = () => {
               Value={CurDate}
               setValue={setCurDate}
             />
+            <AuthInputPopOver
+              label={"Select Station"}
+              placeholder={"Select Station..."}
+              Value={StationName}
+              onClick={(data) => handleClick(data)}
+            />
+
+            <Popover
+              id={id}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={handleClose}
+              PaperProps={{
+                sx: {
+                  borderRadius: "25px", // Add rounded corners
+                  backgroundColor: "white", // Set background color to white
+                  width: "300px", // Set the width as needed
+                  overflow: "hidden", // Hide overflowing content
+                  //   marginTop: "6px",
+                },
+              }}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "right",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+            >
+              <Typography
+                sx={{
+                  p: 2,
+                  borderColor: "#465462",
+                  backgroundColor: "#465462",
+                  width: "400px",
+                  overflow: "hidden",
+                  borderRadius: "25px",
+                }}
+              >
+                <div className="bg-[#465462] text-white font-[Quicksand]  flex flex-col justify-center items-center rounded-[50px]">
+                  <div className="w-full flex flex-col justify-between gap-y-3 items-start">
+                    {StationsData.data.map((data) => {
+                      return (
+                        <div
+                          className="flex gap-x-3 items-center cursor-pointer"
+                          onClick={() => {
+                            handleClose();
+                            setStationId(data._id);
+                            setStationName(data.name);
+                          }}
+                        >
+                          <input
+                            type="checkbox"
+                            className="mr-1 appearance-none h-5 w-5 border border-gray-300 checked:bg-white rounded-full"
+                            checked={StationId === data._id}
+                          />
+                          <span>{data.name}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              </Typography>
+            </Popover>
             <div className="flex border-[1px] w-[300px] border-black items-center gap-x-2 px-3 py-[6px] rounded-full overflow-hidden max767:hidden">
               <BsSearch />
               <input
